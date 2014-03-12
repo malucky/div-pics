dfea46c1ff3098fec728d293e2591e288f47c0d2
Ray Ma <rhm262@gmail.com>
2014-03-12 12:14:55 -0700

    combine horizontal divs if same color

--- pixel.js
+++ pixel.js
@@ -20,12 +20,25 @@ getPixels("test.png", function (err, pixels) {
     console.log("Bad image path");
     return;
   }
-  for (var i = 0; i < pixels.data.byteLength; i += 4) {
-    var top = ~~ (i / pixels._shape1 / 4 * pixSize) + 'px';
-    var left = ((i / 4) % (pixels._shape1)) * pixSize + 'px';
-    var color = 'rgba(' + pixels.data[i] + ',' + pixels.data[i + 1] + ',' + pixels.data[i + 2] + ',' + pixels.data[i + 3] / 255 + ')';
-    str += "<div style=top:" + top + ";left:" + left + ";background-color:" + color + ";></div>";
-    if (i % 400 === 0) console.log(i / pixels.data.byteLength * 100 + '%');
+  var data = pixels.data;
+  var length = data.byteLength;
+  var width = pixSize;
+  var temp = {};
+  for (var i = 0; i < length; i += 4) {
+    //if same
+    if (data[i] === data[i - 4] && data[i + 1] === data[i - 3] && data[i + 2] === data[i - 2] && data[i + 3] === data[i - 1]) {
+      width += pixSize;
+      continue;
+    } else {
+      str += "<div style=width:" + width + "px;top:" + temp.top + "px;left:" + temp.left + "px;background-color:" + temp.color + ";></div>";
+    }
+    //not same
+    temp.top = ~~ (i / pixels._shape1 / 4 * pixSize);
+    temp.left = ((i / 4) % (pixels._shape1)) * pixSize;
+    temp.color = 'rgba(' + data[i] + ',' + data[i + 1] + ',' + data[i + 2] + ',' + data[i + 3] / 255 + ')';
+    width = pixSize;
+
+    if (i % 400 === 0) console.log(i / length * 100 + '%');
   }
   fs.writeFile("index.html", '<html><head><style>div{margin: 0; padding: 0;position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style></head><body>' + str + '</body></html>');
 });
