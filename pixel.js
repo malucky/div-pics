75b6ff60efa52bd72be234691b1b630509ae02f7
Ray Ma <rhm262@gmail.com>
2014-03-12 10:50:55 -0700

    Some optimizations

--- pixel.js
+++ pixel.js
@@ -1,45 +1,45 @@
-var window = {document: {}};
+var window = {
+  document: {}
+};
 var getPixels = require('get-pixels');
 var jsdom = require("jsdom");
 var $ = require('jquery');
 var fs = require('fs');
+var arr = [];
+
+var pixSize = 2;
+
+var writeFile = function (data) {
+  console.log('writing file');
+  fs.writeFile("index.html", data);
+};
 
 jsdom.env(
-  '<div></div>',
+  '<div"></div>',
   // ["http://code.jquery.com/jquery.js"],
   function (errors, window) {
     $ = $(window);
-    console.log($('html').html()); 
-    getPixels("test.png", function(err, pixels) {
-      if(err) {
-        console.log("Bad image path")
-        return
+    console.log($('html').html());
+    getPixels("test.png", function (err, pixels) {
+      if (err) {
+        console.log("Bad image path");
+        return;
       }
       var $html = $('html');
-      // for(var i = 0; i < pixels.data.byteLength; i += 4) {
-      //   var temp = $('<div></div>')
-      //   .css({
-      //     margin: 0,
-      //     padding: 0,
-      //     height: '2px',
-      //     width: '2px',
-      //     position: 'absolute',
-      //     top: Math.floor(i / (pixels._shape1 * 4)) * 2 + 'px',
-      //     left: (i % (pixels._shape2* 4))/4  * 2+ 'px',
-      //     'background-color': 'rgba(' + pixels.data[i] + ',' + pixels.data[i + 1] + ',' + pixels.data[i + 2]+ ','  + pixels.data[i + 3] / 255+ ')'
-      //   });
-      //   $html.append(temp);
-      //   if(i % 40 === 0){
-      //     console.log(i);
-      //   }
-      // }
+      $html.append('<style>div{position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style>');
+      for (var i = 0; i < pixels.data.byteLength; i += 4) {
+        var temp = $('<div></div>')
+          .css({
+            top: Math.floor(i / (pixels._shape1 * 4)) * 2 + 'px',
+            left: (i % (pixels._shape2 * 4)) / 4 * 2 + 'px',
+            'background-color': 'rgba(' + pixels.data[i] + ',' + pixels.data[i + 1] + ',' + pixels.data[i + 2] + ',' + pixels.data[i + 3] / 255 + ')'
+          });
+        arr.push(temp);
+        if (i % 400 === 0) console.log(i / pixels.data.byteLength * 100 + '%');
+      }
+      $html.append(arr);
       writeFile($html.html());
-      console.log(pixels.data);
     });
 
   }
 );
-var writeFile = function (data) {
-  fs.writeFile("index.html", data);
-}
-
