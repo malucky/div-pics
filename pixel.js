f95404662a5e32030e3f933a9d523a059ba71c30
Ray Ma <rhm262@gmail.com>
2014-03-12 11:25:52 -0700

    Refactored to string

--- pixel.js
+++ pixel.js
@@ -14,32 +14,18 @@ var writeFile = function (data) {
   fs.writeFile("index.html", data);
 };
 
-jsdom.env(
-  '<div"></div>',
-  // ["http://code.jquery.com/jquery.js"],
-  function (errors, window) {
-    $ = $(window);
-    console.log($('html').html());
-    getPixels("test.png", function (err, pixels) {
-      if (err) {
-        console.log("Bad image path");
-        return;
-      }
-      var $html = $('html');
-      $html.append('<style>div{position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style>');
-      for (var i = 0; i < pixels.data.byteLength; i += 4) {
-        var temp = $('<div></div>')
-          .css({
-            top: Math.floor(i / (pixels._shape1 * 4)) * 2 + 'px',
-            left: (i % (pixels._shape2 * 4)) / 4 * 2 + 'px',
-            'background-color': 'rgba(' + pixels.data[i] + ',' + pixels.data[i + 1] + ',' + pixels.data[i + 2] + ',' + pixels.data[i + 3] / 255 + ')'
-          });
-        arr.push(temp);
-        if (i % 400 === 0) console.log(i / pixels.data.byteLength * 100 + '%');
-      }
-      $html.append(arr);
-      writeFile($html.html());
-    });
-
+var str = '';
+getPixels("test.png", function (err, pixels) {
+  if (err) {
+    console.log("Bad image path");
+    return;
   }
-);
+  for (var i = 0; i < pixels.data.byteLength; i += 4) {
+    var top = ~~ (i / pixels._shape1 / 4 * pixSize) + 'px';
+    var left = ((i / 4) % (pixels._shape1)) * pixSize + 'px';
+    var color = 'rgba(' + pixels.data[i] + ',' + pixels.data[i + 1] + ',' + pixels.data[i + 2] + ',' + pixels.data[i + 3] / 255 + ')';
+    str += "<div style=top:" + top + ";left:" + left + ";background-color:" + color + ";></div>";
+    if (i % 400 === 0) console.log(i / pixels.data.byteLength * 100 + '%');
+  }
+  fs.writeFile("index.html", '<html><head><style>div{margin: 0; padding: 0;position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style></head><body>' + str + '</body></html>');
+});
