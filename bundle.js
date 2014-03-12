e92d5bdf494df1c5d77692cad83f113ff61f7d47
Alex Taylor <alexmckenley@gmail.com>
2014-03-12 15:44:38 -0700

    5px browserify implementation

--- bundle.js
+++ bundle.js
@@ -394,38 +394,40 @@ function iota(n) {
 
 module.exports = iota
 },{}],4:[function(require,module,exports){
-
 var getPixels = require('get-pixels');
 
-var pixSize = 2;
+var pixSize = 4;
 
-getPixels("test.png", function (err, pixels) {
-  if (err) {
-    console.log("Bad image path");
-    return;
-  }
-  console.log("got image data");
-  var data = pixels.data;
-  var length = data.byteLength;
-  var width = pixSize;
-  var temp = {};
-  for (var i = 0; i < length; i += 4) {
-    //if same
-    if (data[i] === data[i - 4] && data[i + 1] === data[i - 3] && data[i + 2] === data[i - 2] && data[i + 3] === data[i - 1]) {
-      width += pixSize;
-      continue;
-    } else {
-      $(document.body).append("<div style=width:" + width + "px;top:" + temp.top + "px;left:" + temp.left + "px;background-color:" + temp.color + ";></div>");
+$(document).ready(function () {
+
+  getPixels("test.png", function (err, pixels) {
+    if (err) {
+      console.log("Bad image path");
+      return;
     }
-    //not same
-    temp.top = Math.floor(i / (pixels._shape1 * 4)) * pixSize,
-    temp.left = ((i / 4) % (pixels._shape1)) * pixSize;
-    temp.color = 'rgba(' + data[i] + ',' + data[i + 1] + ',' + data[i + 2] + ',' + data[i + 3] / 255 + ')';
-    width = pixSize;
+    console.log("got image data");
+    var data = pixels.data;
+    var length = data.byteLength;
+    var width = pixSize;
+    var temp = {};
+    for (var i = 0; i < length; i += 4) {
+      //if same
+      if (data[i] === data[i - 4] && data[i + 1] === data[i - 3] && data[i + 2] === data[i - 2] && data[i + 3] === data[i - 1]) {
+        width += pixSize;
+        continue;
+      } else {
+        $(document.body).append("<div style='width:" + width + "px;height:" + pixSize + "px;top:" + temp.top + "px;left:" + temp.left + "px;background-color:" + temp.color + ";'></div>");
+      }
+      //not same
+      temp.top = Math.floor(i / (pixels._shape1 * 4)) * pixSize,
+      temp.left = ((i / 4) % (pixels._shape1)) * pixSize;
+      temp.color = 'rgba(' + data[i] + ',' + data[i + 1] + ',' + data[i + 2] + ',' + data[i + 3] / 255 + ')';
+      width = pixSize;
 
-    if (i % 400 === 0) console.log(i / length * 100 + '%');
-  }
-  // fs.writeFile("index.html", '<html><head><style>div{margin: 0; padding: 0;position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style></head><body>' + str + '</body></html>');
+      if (i % 400 === 0) console.log(i / length * 100 + '%');
+    }
+    // fs.writeFile("index.html", '<html><head><style>div{margin: 0; padding: 0;position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style></head><body>' + str + '</body></html>');
+  });
 });
 },{"get-pixels":1}],5:[function(require,module,exports){
 /**
