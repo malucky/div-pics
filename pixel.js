49ef5c988a0fd01c4f925f297aaeae6fbda55124
Alex Taylor <alexmckenley@gmail.com>
2014-03-12 17:16:47 -0700

    using css3 transforms

--- pixel.js
+++ pixel.js
@@ -1,10 +1,10 @@
 var getPixels = require('get-pixels');
 
-var pixSize = 8;
+var pixSize = 4;
 
 $(document).ready(function () {
 
-  getPixels("tux.png", function (err, pixels) {
+  getPixels("test.png", function (err, pixels) {
     if (err) {
       console.log("Bad image path");
       return;
@@ -21,7 +21,7 @@ $(document).ready(function () {
     temp.left = ((i / 4) % (pixels._shape1)) * pixSize;
     temp.color = 'rgba(' + data[i] + ',' + data[i + 1] + ',' + data[i + 2] + ',' + data[i + 3] / 255 + ')';
     width = pixSize;
-    for (var i = 1; i < length; i += 4) {
+    for (i = 0; i < length; i += 4) {
       //if same
       if (data[i] === data[i - 4] && data[i + 1] === data[i - 3] && data[i + 2] === data[i - 2] && data[i + 3] === data[i - 1]) {
         width += pixSize;
@@ -31,7 +31,10 @@ $(document).ready(function () {
           width: width,
           top: temp.top,
           left: temp.left,
-          color: temp.color
+          color: temp.color,
+          starty: ~~(Math.random() * 1000),
+          startx: ~~(Math.random() * 1000)
+
         });
         // $(document.body).append("<div style='width:" + width + "px;height:" + pixSize + "px;top:" + temp.top + "px;left:" + temp.left + "px;background-color:" + temp.color + ";'></div>");
 
@@ -45,36 +48,43 @@ $(document).ready(function () {
       if (i % 400 === 0) console.log(i / length * 100 + '%');
     }
 
-    var body = d3.select('body').append('svg');
-    body.attr('height', 1000);
-    body.attr('width', 1000);
-    body.selectAll('rect')
+    var body = d3.select('body').append('div');
+    // body.attr('height', 1000);
+    // body.attr('width', 1000);
+    body.selectAll('div')
       .data(arr)
       .enter()
-      .append('rect')
-      .attr('width', '8px')
-      .attr('y', function (d, i) {
-        return~~ (Math.random() * 1000);
+      .append('div')
+      .style('width', '4px')
+      .style('top', function (d, i) {
+        return d.starty;
       })
-      .attr('x', function (d, i) {
-        return~~ (Math.random() * 1000);
+      .style('left', function (d, i) {
+        return d.startx;
       })
-      .attr('height', '8px')
-      .attr('fill', function (d, i) {
+      .style('height', '4px')
+      .style('background-color', function (d, i) {
         return d.color;
       })
-      .transition()
-      .duration(7000)
-      .attr('y', function (d, i) {
-        return d.top;
-      })
-      .attr('x', function (d, i) {
-        return d.left;
-      })
-      .attr('width', function (d, i) {
+
+    setTimeout(function () {
+
+
+
+      body.selectAll('div')
+        .style('-webkit-transform', function (d, i) {
+          var offsetX = d.left - d.startx;
+          var offsetY = d.top - d.starty;
+          return 'translate(' + offsetX + 'px,' + offsetY + 'px)';
+        })
+      // .style('left', function (d, i) {
+      //   return d.left;
+      // })
+      .style('width', function (d, i) {
         return d.width;
       });
 
+    }, 0);
     // fs.writeFile("index.html", '<html><head><style>div{margin: 0; padding: 0;position:absolute;padding:0;margin:0;height:' + pixSize + 'px;width:' + pixSize + 'px;}</style></head><body>' + str + '</body></html>');
   });
 });
